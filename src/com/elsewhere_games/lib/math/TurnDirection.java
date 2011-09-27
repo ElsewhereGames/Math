4a5fab611481b377ded32e2ad98ccad44d5068d1
Hans Pragt <ElsewhereGames@gmail.com>
2011-09-26 21:35:40 -0700

    Added comments clarifying use of the TurnDirection enumeration.

--- src/com/elsewhere_games/lib/math/TurnDirection.java
+++ src/com/elsewhere_games/lib/math/TurnDirection.java
@@ -2,8 +2,14 @@ package com.elsewhere_games.lib.math;
 
 public enum TurnDirection {
 
+	/**
+	 * <p>Indicates a clockwise turn.</p>
+	 */
 	CLOCKWISE,
 	
+	/**
+	 * <p>Indicates a counter-clockwise turn.</p>
+	 */
 	COUNTER_CLOCKWISE
 	
 }
